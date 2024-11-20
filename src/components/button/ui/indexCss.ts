1ff6aa16449b1e23d90ac38de625efd6891a633b
youjin-hong <asdfghjk1052@naver.com>
2024-11-21 00:30:09 +0900

    🎨 [#2] Refactor: style 가이드대로 props 수정

--- src/components/button/ui/indexCss.ts
+++ src/components/button/ui/indexCss.ts
@@ -8,28 +8,27 @@ const ButtonComponent = styled.button<{
   color: string;
   width: string;
   height?: string;
+  fontWeight?: string;
   disabled?: boolean;
 }>`
-  border: ${({ border }) => border || '1px solid #DCDCDC'};
+  border: ${({ border }) => border || 'none'};
   border-radius: ${({ borderRadius }) => borderRadius || '12px'};
   font-size: ${({ fontSize }) => fontSize || '16px'};
   background-color: ${({ bgColor }) => bgColor || '#2382FF'};
-  color: ${({ color }) => color || '#A4A4A4'};
-  width: ${({ width }) => width || '100px'};
-  height: ${({ height }) => height || '40px'};
-
-  font-weight: 600;
+  color: ${({ color }) => color || '#FFFFFF'};
+  width: ${({ width }) => width || '167px'};
+  height: ${({ height }) => height || '47px'};
+  font-weight: ${({ fontWeight }) => fontWeight || 'medium'};
   cursor: pointer;
 
-  // 비활성화 상태 - TODO: 시안 나오면 맞게 수정할 예정
-  opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
+  // 비활성화 상태
   pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
 
-  // TODO: 시안 나오면 맞게 수정할 예정
-  &:hover {
+  // TODO: hover 했을 때, 클릭 됐을 때 경우 나눠서 수정해야 함
+  /* &:hover {
     background-color: ${({ bgColor }) =>
-      bgColor === 'default' ? '#62A6FF' : bgColor === 'emphasize' ? '#2382FF' : ''};
-  }
+    bgColor === 'default' ? '#62A6FF' : bgColor === 'emphasize' ? '#2382FF' : ''};
+  } */
 `;
 
 export default ButtonComponent;
