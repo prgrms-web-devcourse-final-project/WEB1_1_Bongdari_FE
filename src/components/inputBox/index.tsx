c89112521edec96c20120b377cdf9b33cd24db90
jooyoung <79846054+djm06294@users.noreply.github.com>
2024-11-21 15:48:13 +0900

    ✨ [#1] Feat: InputBox 컴포넌트 1차 구현 (#3)
    * ✨ [#1] Feat: InputBox 컴포넌트 1차 구현
    
    - logic: onChange시 text 저장, Enter 누르면 콘솔에 text 출력 c력
    
    * ✨ [#1] Feat: InputBox 수정사항 반영
    
    1. 색상타입 두가지 적용, 2. getInputText 함수 props를 받아서 상위 컴포넌트로 내용 저 전전달, 3. div를div를 input으로 바꾸기.
    
    * :sparkles:[#1] Feat: InputBox props 수정 및 로직정리
    - borderradius props 추가 ('8px', '12px'로 타입 제한)
    - 지저분한 코드 살짝 정리
    
    * 🐛  colortype props 타입 변경

---
+++ src/components/inputBox/index.tsx
@@ -0,0 +1,40 @@
+import InputBoxCss from './indexCss';
+import { useInput } from './logic/useInput';
+
+interface InputCompProps {
+  getInputText: (inputText: string) => void; // inputText 상위전달
+  colortype: 0 | 1; // 색상 타입
+  borderradius?: '8px' | '12px';
+  width: string;
+  height?: string;
+  initialVal?: string; // input내의 초깃값
+  placeholder?: string; // input의 placeholder
+}
+
+const InputBox: React.FC<InputCompProps> = ({
+  getInputText,
+  colortype = 0,
+  borderradius = '8px',
+  width,
+  height = '',
+  initialVal = '',
+  placeholder = '작성하세요.'
+}: InputCompProps) => {
+  const { inputText, onChangeInput, onEnter, onBlur } = useInput({ initialVal, getInputText });
+
+  return (
+    <InputBoxCss
+      width={width}
+      height={height}
+      borderradius={borderradius}
+      colortype={colortype}
+      type="text"
+      value={inputText}
+      placeholder={placeholder}
+      onChange={onChangeInput}
+      onKeyDown={onEnter}
+      onBlur={onBlur}
+    />
+  );
+};
+export default InputBox;
---
+++ src/components/inputBox/indexCss.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+// colortype ? 회색입력창 : 흰색입력창
+interface InputBoxCssProps {
+  colortype: 0 | 1;
+  borderradius: '8px' | '12px';
+  width: string;
+  height: string;
+}
+
+const InputBoxCss = styled.input<InputBoxCssProps>`
+  box-sizing: border-box;
+  outline: none;
+
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  color: '#363636';
+  background-color: ${(props) => (props.colortype ? '#F5F5F5' : '#fff')};
+  border: 1px solid ${(props) => (props.colortype ? 'transparent' : '#dbdbdb')};
+  border-radius: ${(props) => props.borderradius};
+  line-height: 16px;
+  font-size: 16px;
+  padding: 5px;
+
+  &::placeholder {
+    color: ${(props) => (props.colortype ? '#363636' : '#E0E0E0')};
+  }
+
+  &:focus-within {
+    border: 1px solid ${(props) => (props.colortype ? '#E0E0E0' : '#363636')};
+  }
+`;
+export default InputBoxCss;
