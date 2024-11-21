39c477b65bceb6cb33653e19be1ae0d10f179156
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-11-21 12:32:15 +0900

    ✨feat: label component 개발완료 (#23)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

---
+++ src/components/label/MessageLabel/index.tsx
@@ -0,0 +1,7 @@
+import { Wrapper } from './indexCss';
+
+const MessageLabel = () => {
+  return <Wrapper>읽지않음</Wrapper>;
+};
+
+export default MessageLabel;
---
+++ src/components/label/MessageLabel/indexCss.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.span`
+  display: inline-block;
+  background-color: #62a6ff;
+  border-radius: 20px;
+  padding: 3px 7px;
+  color: #ffffff;
+  font-size: 12px;
+`;
