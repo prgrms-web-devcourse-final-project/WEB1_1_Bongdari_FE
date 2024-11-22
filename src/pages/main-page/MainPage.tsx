9b12fed0c2fb82b6191fc70d2ac83a4c8e6f38f7
kimminjoon <annawa6@naver.com>
2024-11-22 15:33:05 +0900

    ✨feat : searchBar 컴포넌트 개발

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,10 +1,3 @@
-import SearchBar from '@/components/search-bar';
-
 export default function MainPage() {
-  return (
-    <>
-      메인페이지입니다.
-      <SearchBar></SearchBar>
-    </>
-  );
+  return <>메인페이지입니다.</>;
 }
