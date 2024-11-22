4597152e1d0b188f13a6e159e67107ddba7b7ad5
kimminjoon <annawa6@naver.com>
2024-11-22 16:46:22 +0900

    ✨feat : searchBar 컴포넌트 개발

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,9 +1,3 @@
-import NonFilterSearchBar from '@/components/search-bar/nonfilter-search';
-
 export default function MainPage() {
-  return (
-    <>
-      메인페이지입니다.<NonFilterSearchBar type={false}></NonFilterSearchBar>
-    </>
-  );
+  return <>메인페이지입니다.</>;
 }
