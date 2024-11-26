c4d66c3ef1b80f30cbf578cd01beeff76faf5f6c
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-11-26 11:01:09 +0900

    ✨[#41]Feat : aidrqdetail centerprofile개발 (#44)
    * ✨feat : aidrqdetail-centerprofile컴포넌트 ui개발 완료
    
    * ✨feat : aidrqdetail-centerprofile feature 개발 완료
    
    ---------
    
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,5 +1,10 @@
+import AidRqDetailCenterProfile from '@/features/aidreq-detail-center-profile';
 import { Wrapper } from './MainpageCss';
 
 export default function MainPage() {
-  return <Wrapper>메인페이지입니다.</Wrapper>;
+  return (
+    <Wrapper>
+      메인페이지입니다.<AidRqDetailCenterProfile></AidRqDetailCenterProfile>
+    </Wrapper>
+  );
 }
