b6bd864bcb2ead3bf898dde31bfcbe48fca0c3b1
youjin-hong <asdfghjk1052@naver.com>
2024-11-27 02:13:30 +0900

    ✨ [#58] Feat: 기관 마이페이지 기본 ui, 기본 동작 구현 완료

--- src/features/register-goods/_components/register-bar/indexCss.ts
+++ src/features/register-goods/_components/register-bar/indexCss.ts
@@ -9,13 +9,19 @@ export const RegisterBarContainer = styled.div`
 export const RegisterButton = styled.button`
   width: 221px;
   height: 53px;
-  border: 1px solid ${theme.pointColor.clicked};
+  border: 1px solid ${theme.pointColor.event};
   border-radius: 13px;
   background-color: #ffffff;
-  color: ${theme.pointColor.clicked};
+  color: ${theme.pointColor.event};
   font-size: ${theme.fontSize.eighthSize};
   cursor: pointer;
   font-weight: 600;
+  transition: 0.3s;
+
+  &:active {
+    color: ${theme.pointColor.clicked};
+    border: 1px solid ${theme.pointColor.clicked};
+  }
 `;
 
 export const RegisterInput = styled.input`
