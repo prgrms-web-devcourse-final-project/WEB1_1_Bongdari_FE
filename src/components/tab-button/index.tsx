081011078c6b565a936f602ceb709dfa8b6cdf61
jooyoung <jykimjbr860@gmail.com>
2024-11-26 21:19:54 +0900

    ✨ [#53] Feat: tab-button 컴포넌트에 height, borderRadius props 추가

--- src/components/tab-button/index.tsx
+++ src/components/tab-button/index.tsx
@@ -7,9 +7,21 @@ type TabButtonGroupProps = {
   tabs: TabButtonData[]; // 버튼 데이터 배열 - 탭 버튼 개수 설정하기 위한 타입
   initialActiveTab?: string; // default 활성화 탭 버튼 (옵션)
   onTabChange?: (activeTab: string) => void; // 탭 변경 시 실행할 콜백 함수 (옵션)
+  width?: string;
+  height?: string;
+  fontSize?: string;
+  borderRadius?: string;
 };
 
-const TabButtonGroup = ({ tabs, initialActiveTab, onTabChange }: TabButtonGroupProps) => {
+const TabButtonGroup = ({
+  tabs,
+  initialActiveTab,
+  onTabChange,
+  width = '167px',
+  height,
+  fontSize = '16px',
+  borderRadius
+}: TabButtonGroupProps) => {
   const { activeTab, handleTabClick } = useTabButtonGroup({
     tabs,
     initialActiveTab,
@@ -23,8 +35,10 @@ const TabButtonGroup = ({ tabs, initialActiveTab, onTabChange }: TabButtonGroupP
           key={tab.label}
           label={tab.label}
           onClick={() => handleTabClick(tab.label)}
-          fontSize="16px"
-          width="167px"
+          fontSize={fontSize}
+          width={width}
+          height={height}
+          borderRadius={borderRadius || ''}
           variant={activeTab === tab.label ? `clicked` : `notClicked`}
         />
       ))}
