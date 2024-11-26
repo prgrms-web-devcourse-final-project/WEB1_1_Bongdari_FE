867f7202fe862e11a0b497c2647d2878698bdcef
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-11-26 14:37:32 +0900

    ✨feat : aidrq detail info feature ui 개발 완료 (#47)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

---
+++ src/features/aidreq-detail-info/_components/location/indexCss.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+
+export const Wrapper = styled.div`
+  flex: 1;
+
+  & > h2 {
+    font-size: 24px;
+    font-weight: 700;
+    padding: 20px 0;
+  }
+`;
+
+export const Container = styled.div`
+  border: ${theme.box.section.border};
+  border-radius: ${theme.box.section.borderRadius};
+  height: 150px;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+
+  & > div:nth-of-type(1) {
+    height: 100px;
+    border-bottom: 1px solid #d1d1d1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    & > p {
+      font-weight: 500;
+    }
+  }
+
+  & > div:nth-of-type(2) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    transition: 0.2s;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.1);
+      cursor: pointer;
+    }
+
+    & > p {
+      color: #555555;
+      font-weight: 400;
+    }
+  }
+`;
