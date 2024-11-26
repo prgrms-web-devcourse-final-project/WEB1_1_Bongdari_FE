7006600012ef0f831c015024423580e43d76272c
jooyoung <jykimjbr860@gmail.com>
2024-11-26 10:30:06 +0900

    🐛 [#42] inputBox props추가 및 Css 변경

--- src/components/inputBox/index.tsx
+++ src/components/inputBox/index.tsx
@@ -4,36 +4,40 @@ import { useInput } from './logic/useInput';
 interface InputCompProps {
   getInputText: (inputText: string) => void; // inputText 상위전달
   colortype: 0 | 1; // 색상 타입
-  borderradius?: '8px' | '12px';
+  textType?: 'text' | 'password';
   width: string;
   height?: string;
   initialVal?: string; // input내의 초깃값
   placeholder?: string; // input의 placeholder
+  isDisabled?: boolean;
+  setFunc?: (inputText: string) => void; // 상위의 set함수
 }
 
 const InputBox: React.FC<InputCompProps> = ({
   getInputText,
   colortype = 0,
-  borderradius = '8px',
+  textType = 'text',
   width,
   height = '',
   initialVal = '',
-  placeholder = '작성하세요.'
+  placeholder = '작성하세요.',
+  isDisabled = false,
+  setFunc
 }: InputCompProps) => {
-  const { inputText, onChangeInput, onEnter, onBlur } = useInput({ initialVal, getInputText });
+  const { inputText, onChangeInput, onEnter, onBlur } = useInput({ initialVal, getInputText, setFunc });
 
   return (
     <InputBoxCss
       width={width}
       height={height}
-      borderradius={borderradius}
       colortype={colortype}
-      type="text"
+      type={textType}
       value={inputText}
       placeholder={placeholder}
       onChange={onChangeInput}
       onKeyDown={onEnter}
       onBlur={onBlur}
+      disabled={isDisabled}
     />
   );
 };
