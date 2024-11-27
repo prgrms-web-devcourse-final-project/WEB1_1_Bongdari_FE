70e8c1e4e23ee15bb9827338c956108479dc8d88
jooyoung <jykimjbr860@gmail.com>
2024-11-27 11:49:40 +0900

    ✨ [#60] feat: profile에서 쪽지보내기 modal 컴포넌트 구현

---
+++ src/features/profile-page-mail-modal/indexCss.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+
+export const ProfileMailModalCss = styled.div`
+  .modalInnerWrap {
+    padding: 45px;
+  }
+
+  .modalInnerWrap > i {
+    font-size: ${theme.fontSize.secondSize};
+    font-weight: 600;
+    padding-bottom: 40px;
+    display: inline-block;
+  }
+
+  .inputWrap {
+    display: flex;
+    padding-bottom: 20px;
+  }
+  .inputWrap > i {
+    font-size: ${theme.fontSize.eighthSize};
+    font-weight: 600;
+    color: #656565;
+
+    width: 60px;
+    padding-top: 16px;
+  }
+
+  .btnWrap {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    gap: 10px;
+  }
+  .btnWrap .checkErr {
+    font-size: ${theme.fontSize.eighthSize};
+    font-weight: 500;
+    color: red;
+    opacity: 0.8;
+  }
+`;
