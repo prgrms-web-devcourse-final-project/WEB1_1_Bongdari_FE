fd460d3b2659f6b4c72c61fbd091b91165ad5fa2
jooyoung <jykimjbr860@gmail.com>
2024-11-27 16:29:59 +0900

    ✨ [#67] Feat: personal my page 컴포넌트 수정

--- src/features/personal-my-page-feature/_components/heart-org-list/index.tsx
+++ src/features/personal-my-page-feature/_components/heart-org-list/index.tsx
@@ -2,29 +2,41 @@ import { HeartedOrgCss } from './indexCss';
 import SectionTitle from '../section-title';
 import OrgBox from './org-box/OrgBox';
 import { useEffect, useState } from 'react';
+import { heartedOrgType } from '@/shared/types/person-profile/personProfile';
 
-const HeartedOrg = ({ data }: { data: string[] }) => {
-  // TODO: 데이터 props로 받아오고 뿌려주기.
-  // TODO: pagenation 생각해보기
+const HeartedOrg = ({ user_id }: { user_id: string }) => {
   const [totPage, setTotPage] = useState<number>(0);
   const [currPage, setCurrPage] = useState<number>(1);
+  const tmpdata: heartedOrgType[] = [
+    { id: 'id1', name: 'name1' },
+    { id: 'id1', name: 'name1' },
+    { id: 'id1', name: 'name1' },
+    { id: 'id1', name: 'name1' },
+    { id: 'id1', name: 'name1' },
+    { id: 'id1', name: 'name1' },
+    { id: 'id1', name: 'name1' }
+  ];
 
-  // TODO: width를 계산해서 몇개 요소 들어갈지 pagenation 구하기
   useEffect(() => {
+    // TODO: user_id를 가지고 데이터 불러오기
+    // TODO: 받아오는 data의 타입을 지정해주기(api 보고 타입 지정하기)
+    console.log('TODO: ', user_id, '의 관심기관 조회');
+
+    // TODO: width를 계산해서 몇개 요소 들어갈지 pagenation 구하기
     let countPerPage = 7;
     const w = window.innerWidth;
     console.log('width', w);
     if (w < 1200 && w >= 1000) countPerPage = 6;
     else if (w >= 800) countPerPage = 5;
-    setTotPage(Math.ceil(data.length / countPerPage));
-  }, [data]);
+    setTotPage(Math.ceil(tmpdata.length / countPerPage));
+  }, []);
 
   return (
     <HeartedOrgCss>
       <SectionTitle title="내 관심 기관" totPage={totPage} currPage={currPage} setCurrPage={setCurrPage} />
       <div className="listWrap">
-        {data.map((v) => (
-          <OrgBox org_id="123" orgName={v} onClickFunc={(id: string) => console.log(id)} />
+        {tmpdata.map((v) => (
+          <OrgBox org_id={v.id} orgName={v.name} onClickFunc={(id: string) => console.log(id)} />
         ))}
       </div>
     </HeartedOrgCss>
