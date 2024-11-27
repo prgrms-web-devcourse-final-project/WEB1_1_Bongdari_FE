3dbeee09dc965a9beafb351450c71e9b712bf856
Minjoon Kim <annawa678@gmail.com>
2024-11-27 16:12:25 +0900

    ✨feat : header 개발 완료

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,7 +1,26 @@
+import { Link } from 'react-router-dom';
+import { Contents, Logo, Menu, Wrapper } from './HeaderCss';
+
 export default function Header() {
   return (
-    <>
-      <p>헤더입니다.</p>
-    </>
+    <Wrapper>
+      <Contents>
+        <Logo>SOMEMORE</Logo>
+        <Menu>
+          <Link to="/aidrqlist" className="link">
+            <li>활동구인</li>
+          </Link>
+          <Link to="/main" className="link">
+            <li>주변활동찾기</li>
+          </Link>
+          <Link to="/main" className="link">
+            <li>커뮤니티</li>
+          </Link>
+          <Link to="/main" className="link">
+            <li>마이페이지</li>
+          </Link>
+        </Menu>
+      </Contents>
+    </Wrapper>
   );
 }
