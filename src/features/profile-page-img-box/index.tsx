ccc1f7502e6f8aebfcda2b47274add3d984b8f1d
jooyoung <jykimjbr860@gmail.com>
2024-11-27 11:41:57 +0900

    ✨ [#60] Feat: 쪽지 보내기 버튼 클릭시 모달 열리도록 구현

--- src/features/profile-page-img-box/index.tsx
+++ src/features/profile-page-img-box/index.tsx
@@ -1,13 +1,13 @@
 import { SubmitButton } from '@/components/button';
 import { ProfileImgBoxCss } from './indexCss';
-import { handleClickMail } from './logic/handleClickMail';
 import { personProfileType } from '@/shared/types/person-profile/personProfile';
 
 interface ProfileImgBoxProps extends personProfileType {
   user_id: string;
+  setIsModalOpen: (bool: boolean) => void;
 }
 
-const ProfileImgBox: React.FC<ProfileImgBoxProps> = ({ user_id, imgUrl, nickname, tier }) => {
+const ProfileImgBox: React.FC<ProfileImgBoxProps> = ({ user_id, imgUrl, nickname, tier, setIsModalOpen }) => {
   return (
     <ProfileImgBoxCss id={user_id}>
       <img src={imgUrl} alt="" />
@@ -15,7 +15,7 @@ const ProfileImgBox: React.FC<ProfileImgBoxProps> = ({ user_id, imgUrl, nickname
         <i>{nickname}</i>
         <img className="mitten" src={`/assets/imgs/mitten-${tier}.svg`} />
       </p>
-      <SubmitButton label="쪽지 전달하기" onClick={() => handleClickMail(user_id)} />
+      <SubmitButton label="쪽지 전달하기" onClick={() => setIsModalOpen(true)} />
     </ProfileImgBoxCss>
   );
 };
--- src/features/profile-page-img-box/logic/handleClickMail.ts
+++
@@ -1,4 +0,0 @@
-export const handleClickMail = (user_id: string) => {
-  // TODO: 해당 유저 id에게 보내는 쪽지 페이지로 이동.
-  console.log(user_id, '에게 보내는 쪽지 페이지로 이동');
-};
