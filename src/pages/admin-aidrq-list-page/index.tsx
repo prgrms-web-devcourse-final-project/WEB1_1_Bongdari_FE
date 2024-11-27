2e038fac680cd5d292da5e15029af88db2fd0bc1
youjin-hong <asdfghjk1052@naver.com>
2024-11-27 19:47:59 +0900

    ✨ [#69] Test: adminSearchStore 사용하는지 확인 완료

--- src/pages/admin-aidrq-list-page/index.tsx
+++ src/pages/admin-aidrq-list-page/index.tsx
@@ -3,6 +3,10 @@ import WriteAidReqButtonComponent from './_components/writeAidReqButtonComponent
 import { PageWrapper, Title } from './indexCss';
 import TabButtonGroup from '@/components/tab-button';
 import AidRqAdminListWrapper from './_components/aidrqlistadmin-wrapper';
+import { useLocation } from 'react-router-dom';
+import useSearchStore from '@/store/stores/search/searchStore';
+import useAdminSearchStore from '@/store/stores/admin-search/searchStore';
+import { useEffect } from 'react';
 
 const AdminAidRqListPage = () => {
   const tabs = [{ label: '모집중' }, { label: '모집완료' }, { label: '종료' }];
@@ -15,6 +19,24 @@ const AdminAidRqListPage = () => {
     // api 호출 로직 와야 함
     console.log('요청 검색 실행');
   };
+
+  // store 확인용 - 삭제 예정 ----------------------------------------
+  const location = useLocation();
+
+  const searchState = useSearchStore((state) => state);
+  const adminSearchState = useAdminSearchStore((state) => state);
+
+  useEffect(() => {
+    console.log('Search Store State:', searchState.keyword);
+    console.log('Admin Search Store State:', adminSearchState.keyword);
+  }, [searchState, adminSearchState]);
+
+  console.log('현재 경로', location.pathname);
+  console.log(
+    '사용하고 있는 store 확인:',
+    location.pathname === '/aidrqlist' ? 'AidRqList Store' : 'AdminAidRqList Store'
+  );
+  // -----------------------------------------------------------------
 
   return (
     <PageWrapper>
