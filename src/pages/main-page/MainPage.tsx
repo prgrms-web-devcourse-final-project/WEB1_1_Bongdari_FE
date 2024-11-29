e94fcc79ed7d725ae9d0bc2ba7088d592ffb499b
youjin-hong <asdfghjk1052@naver.com>
2024-11-29 23:09:01 +0900

    test

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -6,21 +6,15 @@ import Ranking from './_components/ranking';
 import Community from './_components/community';
 
 export default function MainPage() {
-  const [address, setAddress] = useState<string>('');
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const handleSaveAddress = (newAddress: string) => {
-    setAddress(newAddress); // 저장된 주소를 업데이트
-  };
-
-  const handleAddressModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
   return (
-    <>
-      메인페이지입니다.
-      <Button label="버튼이다" />
-    </>
+    <Wrapper>
+      <MainBanner></MainBanner>
+      <WriteAidReqButtonComponent></WriteAidReqButtonComponent>
+      <AidRqRecentList></AidRqRecentList>
+      <RankAndCommu>
+        <Ranking></Ranking>
+        <Community></Community>
+      </RankAndCommu>
+    </Wrapper>
   );
 }
