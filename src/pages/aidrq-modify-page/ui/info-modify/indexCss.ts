95e00c683a932a2a5db40d238eb745b7d173ee3a
Minjoon Kim <annawa678@gmail.com>
2024-11-29 11:33:12 +0900

    ✨feat : 도움요청글 수정페이지 ui 개발 완료

---
+++ src/pages/aidrq-modify-page/ui/info-modify/indexCss.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components';
+
+import theme from '@/styles/theme';
+
+export const Wrapper = styled.div`
+  margin-top: 10px;
+  border: ${theme.box.section.border};
+  border-radius: ${theme.box.section.borderRadius};
+  background-color: white;
+  padding: 50px;
+
+  & > button {
+    width: 220px;
+    height: 53px;
+    color: white;
+    font-weight: 600;
+    border-radius: 12px;
+    border: none;
+    outline: none;
+    background-color: #2382ff;
+    font-size: 1rem;
+  }
+`;
+
+export const ThirdLine = styled.div`
+  padding-top: 16px;
+  display: flex;
+  gap: 20px;
+
+  & > div:nth-of-type(1) {
+    width: 20%;
+  }
+
+  & > div:nth-of-type(2) {
+    flex: 1;
+  }
+
+  & > div:nth-of-type(3) {
+    flex: 1;
+  }
+
+  & > div {
+    & > p {
+      font-size: 12px;
+      padding-bottom: 10px;
+    }
+  }
+`;
+
+export const TextAreaContainer = styled.div`
+  padding-top: 16px;
+  & > p {
+    font-size: 12px;
+    padding-bottom: 10px;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+
+  & > button {
+    width: 220px;
+    height: 53px;
+    color: white;
+    font-weight: 600;
+    border-radius: 12px;
+    border: none;
+    outline: none;
+    background-color: #2382ff;
+    font-size: 1rem;
+  }
+`;
--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,11 +1,10 @@
-import AidRqCreatePage from '../aidrq-create-page';
+import AidRqModifyPage from '../aidrq-modify-page';
 import { Wrapper } from './MainpageCss';
 
 export default function MainPage() {
   return (
     <Wrapper>
-      메인페이지입니다.
-      <AidRqCreatePage></AidRqCreatePage>
+      메인페이지입니다.<AidRqModifyPage></AidRqModifyPage>
     </Wrapper>
   );
 }
