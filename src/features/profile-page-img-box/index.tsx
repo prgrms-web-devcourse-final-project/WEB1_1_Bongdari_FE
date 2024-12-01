ec4206fa5c62ea8f06248e9eb99c0189ca821f6f
jooyoung <jykimjbr860@gmail.com>
2024-12-01 17:06:51 +0900

    ✨ [#76] center profile page에 들어갈 feature 만들기 (person profile 컴포넌트 고치기)

--- src/features/profile-page-img-box/index.tsx
+++ src/features/profile-page-img-box/index.tsx
@@ -1,22 +1,50 @@
 import { SubmitButton } from '@/components/button';
 import { ProfileImgBoxCss } from './indexCss';
 import { personProfileType } from '@/shared/types/person-profile/personProfile';
+import { centerProfileType } from '@/shared/types/center-profile/centerProfile';
 
-interface ProfileImgBoxProps extends personProfileType {
-  user_id: string;
+type personOrCenter = ({ type: 'person' } & personProfileType) | ({ type: 'center' } & centerProfileType);
+
+type ProfileImgBoxProps = {
   setIsModalOpen: (bool: boolean) => void;
-}
+} & personOrCenter;
 
-const ProfileImgBox: React.FC<ProfileImgBoxProps> = ({ user_id, imgUrl, nickname, tier, setIsModalOpen }) => {
-  return (
-    <ProfileImgBoxCss id={user_id}>
-      <img src={imgUrl} alt="" />
-      <p>
-        <i>{nickname}</i>
-        <img className="mitten" src={`/assets/imgs/mitten-${tier}.svg`} />
-      </p>
-      <SubmitButton label="쪽지 전달하기" onClick={() => setIsModalOpen(true)} />
-    </ProfileImgBoxCss>
-  );
+const ProfileImgBox: React.FC<ProfileImgBoxProps> = (props) => {
+  const { setIsModalOpen } = props;
+
+  // 타입 가드 함수 생성
+  const isPersonProfile = (props: personOrCenter): props is { type: 'person' } & personProfileType => {
+    return props.type === 'person';
+  };
+
+  // 조건부 렌더링 및 타입 좁히기
+  if (isPersonProfile(props)) {
+    const { id, nickname, imgUrl, tier } = props;
+
+    return (
+      <ProfileImgBoxCss id={id}>
+        <img src={imgUrl} alt="" />
+        <p>
+          <i>{nickname}</i>
+          <img className="mitten" src={`/assets/imgs/mitten-${tier}.svg`} />
+        </p>
+        <SubmitButton label="쪽지 전달하기" onClick={() => setIsModalOpen(true)} />
+      </ProfileImgBoxCss>
+    );
+  } else {
+    const { center_id, name, homepage_link, imgUrl, interest } = props;
+    console.log(center_id, interest);
+
+    return (
+      <ProfileImgBoxCss id={name}>
+        <img src={imgUrl} alt="" />
+        <p>
+          <i>{name}</i>
+          <i>{homepage_link}</i>
+        </p>
+        <SubmitButton label="쪽지 전달하기" onClick={() => setIsModalOpen(true)} />
+      </ProfileImgBoxCss>
+    );
+  }
 };
 export default ProfileImgBox;
