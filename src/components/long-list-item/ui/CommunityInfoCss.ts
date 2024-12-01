a3ced465a10152eafffd5e8aec2f24682b3bfb5c
jooyoung <jykimjbr860@gmail.com>
2024-12-02 02:21:13 +0900

    ♻️ LongListItem 폴더명 변경

---
+++ src/components/long-list-item/ui/CommunityInfoCss.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const CommunityInfoCss = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 22%;
+
+  .writer,
+  .modifiedDate {
+    margin: 0 10px;
+    text-align: center;
+    color: #808080;
+    font-weight: 300;
+    font-size: 14px;
+    width: 100px;
+  }
+  .writer {
+    color: #a4a4a4;
+
+    /* 길어지면 말줄임표 */
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export default CommunityInfoCss;
