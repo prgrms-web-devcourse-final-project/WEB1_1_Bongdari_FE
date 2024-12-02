3d9b8250ce5199c10654f0a8cc1e24016e2f3501
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-03 00:54:33 +0900

    🐛bug:CICD오류 디버깅 (#120)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/pages/admin-aidrq-list-page/_components/aidrqlistadmin-wrapper/index.tsx
+++ src/pages/admin-aidrq-list-page/_components/aidrqlistadmin-wrapper/index.tsx
@@ -1,28 +1,28 @@
-import { Wrapper, Observer } from './indexCss';
-import { useInfiniteAidRq } from '@/shared/hooks/useInfiniteAidRq';
-import { useInView } from 'react-intersection-observer';
-import { useEffect } from 'react';
+import { Wrapper } from './indexCss';
+// import { useInfiniteAidRq } from '@/shared/hooks/useInfiniteAidRq';
+// import { useInView } from 'react-intersection-observer';
+// import { useEffect } from 'react';
 import AidRqListAdmin from '@/features/aidrq-list-admin';
 
 const AidRqAdminListWrapper = () => {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteAidRq();
+  // const { data, fetchNextPage, hasNextPage } = useInfiniteAidRq();
 
-  const { ref, inView } = useInView();
-  useEffect(() => {
-    if (inView && hasNextPage) {
-      console.log('Fetching next page');
-      fetchNextPage();
-    }
-  }, [inView, hasNextPage, fetchNextPage]);
+  // const { ref, inView } = useInView();
+  // useEffect(() => {
+  //   if (inView && hasNextPage) {
+  //     console.log('Fetching next page');
+  //     fetchNextPage();
+  //   }
+  // }, [inView, hasNextPage, fetchNextPage]);
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
 
   return (
     <Wrapper>
       <AidRqListAdmin></AidRqListAdmin>
-      <Observer ref={ref}></Observer>
+      {/* <Observer ref={ref}></Observer> */}
     </Wrapper>
   );
 };
