6edc8594e289c111fa1d9939a5c9dcc7e866dd4d
jooyoung <jykimjbr860@gmail.com>
2024-12-02 17:22:17 +0900

    ✨ [#110] community create page ui 구현완료

---
+++ src/features/community-create-box/indexCss.ts
@@ -0,0 +1,28 @@
+import theme from '@/styles/theme';
+import styled from 'styled-components';
+
+export const CommunityCreateBoxCss = styled.div`
+  background-color: ${theme.box.section.backgroundColor};
+  border: ${theme.box.section.border};
+  border-radius: ${theme.box.section.borderRadius};
+  padding: 70px;
+
+  .inputWrap {
+    display: grid;
+    grid-template-columns: 60px auto;
+    margin-bottom: 10px;
+  }
+  .inputWrap > .label {
+    font-size: ${theme.fontSize.eighthSize};
+    font-weight: 600;
+    color: #656565;
+    padding-top: 10px;
+    line-height: 30px;
+  }
+
+  .btnWrap {
+    width: 100%;
+    text-align: center;
+    margin-top: 70px;
+  }
+`;
