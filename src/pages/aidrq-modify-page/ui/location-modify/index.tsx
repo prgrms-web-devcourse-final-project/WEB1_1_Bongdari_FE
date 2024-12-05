fda89345b0a0085e2affa68ce7c5f5b67ce2f74a
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-06 01:51:30 +0900

    🐛fix:cicd오류 디버깅 (#175)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/pages/aidrq-modify-page/ui/location-modify/index.tsx
+++ src/pages/aidrq-modify-page/ui/location-modify/index.tsx
@@ -1,13 +1,18 @@
 import AidRqCreateLocation from '@/components/aidrq-create-location';
 import { AidRqCreateLocationWrapper, Contents, Wrapper } from './indexCss';
+import { VolunteerType, Location } from '@/shared/types/aidrq-create-type/AidRqCreateType';
 
-const LocationModify = () => {
+interface LocationModifyProps {
+  getTitleAndFilter: (key: keyof VolunteerType, value: Location) => void;
+}
+
+const LocationModify:React.FC<LocationModifyProps> = ({ getTitleAndFilter }) => {
   return (
     <Wrapper>
       <Contents>
         <p>활동 장소</p>
         <AidRqCreateLocationWrapper>
-          <AidRqCreateLocation></AidRqCreateLocation>
+          <AidRqCreateLocation getTitleAndFilter={getTitleAndFilter}></AidRqCreateLocation>
         </AidRqCreateLocationWrapper>
       </Contents>
       <button>수정하기</button>
