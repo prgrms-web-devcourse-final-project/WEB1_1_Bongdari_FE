3499ad84832969da4fbdb065410302c462c245f9
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-06 02:07:14 +0900

    🐛fix:cicd오류 디버깅 (#177)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/pages/aidrq-modify-page/index.tsx
+++ src/pages/aidrq-modify-page/index.tsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react';
 import { Title, Wrapper } from './indexCss';
 import InfoModify from './ui/info-modify';
 import LocationModify from './ui/location-modify';
+import { VolunteerType, Location } from '@/shared/types/aidrq-create-type/AidRqCreateType';
 
 const AidRqModifyPage = () => {
+  const [volunteerData, setVolunteerData] = useState<VolunteerType>({
+    title: '',
+    content: '',
+    region: '',
+    recruitment_count: 0,
+    volunteer_start_date_time: '',
+    volunteer_end_date_time: '',
+    volunteer_category: '',
+    admitted: false,
+    location: {
+      address: '',
+      latitude: 0,
+      longitude: 0
+    }
+  });
+  useEffect(() => {
+    console.log(volunteerData);
+  }, [volunteerData]);
+
+  const getTitleAndFilter = (key: keyof VolunteerType, value: Location | string | number | boolean) => {
+    setVolunteerData((prev) => ({
+      ...prev,
+      [key]: value
+    }));
+  };
+
   return (
     <Wrapper>
       <Title>도움요청 글 수정</Title>
-      <LocationModify></LocationModify>
+      <LocationModify getTitleAndFilter={getTitleAndFilter}></LocationModify>
       <InfoModify></InfoModify>
     </Wrapper>
   );
