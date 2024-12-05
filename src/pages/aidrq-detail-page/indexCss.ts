a6cd3974d84fbe26551cebdd84d65f95f769977b
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-05 23:37:06 +0900

    ✨[#168]Feat:aidrq디테일페이지 개발 3차 (#169)
    * ✨feat:aidrq상세페이지 api작업 완료
    
    * ✨feat:aidrq상세페이지 api작업 완료
    
    * ✨feat:aidrq상세페이지 api작업 완료
    
    ---------
    
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/pages/aidrq-detail-page/indexCss.ts
+++ src/pages/aidrq-detail-page/indexCss.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+interface PresentResponse {
+  status: string;
+  attended: boolean;
+}
+
 export const Wrapper = styled.div`
   max-width: 1200px;
   width: 90%;
@@ -7,7 +12,7 @@ export const Wrapper = styled.div`
   padding: 250px 0;
 `;
 
-export const ButtonBox = styled.div`
+export const ButtonBox = styled.div<{ presentstate: PresentResponse | null }>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -17,35 +22,35 @@ export const ButtonBox = styled.div`
   & > button:nth-of-type(1) {
     width: 220px;
     height: 53px;
-    border: 1px solid #2382ff;
+    border: 1px solid ${(props) => (props.presentstate?.attended === true ? '#2382ff' : '#E2E2E2')};
     border-radius: 13px;
-    background-color: white;
-    color: #2382ff;
+    background-color: ${(props) => (props.presentstate?.attended === true ? 'white' : '#E2E2E2')};
+    color: ${(props) => (props.presentstate?.attended === true ? '#2382ff' : '#ADADAD')};
     font-size: 14px;
     font-weight: 600;
     transition: 0.2s;
 
     &:hover {
-      cursor: pointer;
-      background-color: #e2efff;
+      cursor: ${(props) => (props.presentstate?.attended === true ? 'pointer' : '')};
+      background-color: ${(props) => (props.presentstate?.attended === true ? '#e2efff' : '#E2E2E2')};
     }
   }
 
   & > button:nth-of-type(2) {
     width: 220px;
     height: 53px;
-    background-color: #2382ff;
+    background-color: ${(props) => (props.presentstate ? '#E2E2E2' : '#2382ff')};
     border: none;
     border-radius: 13px;
     outline: none;
-    color: white;
+    color: ${(props) => (props.presentstate ? '#ADADAD' : 'white')};
     font-size: 14px;
     font-weight: 600;
     transition: 0.2s;
 
     &:hover {
-      cursor: pointer;
-      background-color: #0a66de;
+      cursor: ${(props) => (props.presentstate ? '' : 'pointer')};
+      background-color: ${(props) => (props.presentstate ? '#E2E2E2' : '#0a66de')};
     }
   }
 `;
--- src/pages/main-page/_components/community/logic/fetchMainCommunity.ts
+++ src/pages/main-page/_components/community/logic/fetchMainCommunity.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const fetchMainCommunity = async () => {
-  const response = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/community-boards?page=0&size=6`, {});
+  const response = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/community-boards?page=0`, {});
   return response.data.data;
 };
 
