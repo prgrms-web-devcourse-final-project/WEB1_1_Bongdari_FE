51ca71b0aa8a9933977995617847b8a257587512
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-06 17:18:09 +0900

    ✨feat:로그인구현테스트1차 (#194)

--- src/features/login-feature/_components/org-login/logic/testLoginCenter.ts
+++ src/features/login-feature/_components/org-login/logic/testLoginCenter.ts
@@ -1,23 +1,22 @@
 import axios from 'axios';
-import Cookies from 'js-cookie';
 
-export const testLoginCenter = async () => {
+export const testLoginCenter = async (id: string, pwd: string) => {
   try {
-    const response = await axios.get(
-      `${import.meta.env.VITE_APP_BASE_URL}/api/develop/token/center/919027e3-0d51-4ad3-a80b-e3585179697b`
+    const response = await axios.post(
+      `${import.meta.env.VITE_APP_BASE_URL}/api/center/sign-in`,
+      {
+        account_id: id,
+        account_password: pwd
+      },
+      {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
     );
-    const token = response.data.message;
-    console.log(token);
 
-    // 쿠키에 토큰 저장
-    Cookies.set('centerToken', token, {
-      expires: 7, // 7일 후 만료
-      path: '/', // 모든 경로에서 접근 가능
-      secure: true, // HTTPS에서만 쿠키 전송
-      sameSite: 'strict' // CSRF 방지
-    });
-
-    return response;
+    return response.data;
   } catch (error) {
     console.error('Login failed:', error);
     throw error;
--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import Cookies from 'js-cookie';
+
 import MainBanner from '@/features/main-banner';
 import { RankAndCommu, Wrapper } from './MainpageCss';
 import WriteAidReqButtonComponent from '@/components/write-aidreq-button';
@@ -8,6 +11,29 @@ import { useLoginStore } from '@/store/stores/login/loginStore';
 
 export default function MainPage() {
   const loginType = useLoginStore((state) => state.loginType);
+  const setLoginInfo = useLoginStore((state) => state.setLoginInfo);
+
+  const parseJWT = (token: string) => {
+    try {
+      const base64Payload = token.split('.')[1];
+      const payload = JSON.parse(atob(base64Payload));
+
+      return {
+        id: payload.id,
+        // ROLE_CENTER -> center, ROLE_PERSON -> person 으로 변환
+        role: payload.role.replace('ROLE_', '').toLowerCase()
+      };
+    } catch (error) {
+      console.error('토큰 파싱 실패:', error);
+      return null;
+    }
+  };
+
+  const token = Cookies.get('centerToken') || Cookies.get('personToken');
+
+  useEffect(() => {
+    if (token) setLoginInfo(parseJWT(token)?.id, parseJWT(token)?.role);
+  }, []);
 
   return (
     <Wrapper>
