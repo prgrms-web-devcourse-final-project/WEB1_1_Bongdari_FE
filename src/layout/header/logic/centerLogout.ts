e0a49edb49fd533e956fcd4cfb12314cebd96216
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-08 01:09:51 +0900

    ✨feat:토큰테스트/지원하기등록하기테스트 (#223)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/layout/header/logic/centerLogout.ts
+++ src/layout/header/logic/centerLogout.ts
@@ -1,13 +1,8 @@
 import axiosInstance from '@/api/apis';
-import Cookies from 'js-cookie';
 
 export const centerLogout = async () => {
   try {
-    const response = await axiosInstance.post('/api/center/sign-out', {
-      headers: {
-        Authorization: `${Cookies.get('ACCESS')}`
-      }
-    });
+    const response = await axiosInstance.post('/api/center/sign-out');
     return response;
   } catch (error) {
     console.error('로그아웃 중 오류 발생:', error);
