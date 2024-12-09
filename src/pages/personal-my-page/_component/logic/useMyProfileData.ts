fbd62fd244bf49fb3d6f01cc93f27591db0ca2bd
jooyoung <jykimjbr860@gmail.com>
2024-12-09 11:53:48 +0900

    🐛 [#257] Bug: tier 통일

--- src/pages/personal-my-page/_component/logic/useMyProfileData.ts
+++ src/pages/personal-my-page/_component/logic/useMyProfileData.ts
@@ -14,24 +14,6 @@ export const useMyProfileData = (): useMyProfileDataReturn => {
     const fetchData = async () => {
       const data = await fetchMyProfile();
       if (data && !profileData) setProfileData(data);
-      // else
-      //   setProfileData({
-      //     volunteer_id: '1',
-      //     nickname: 'jooyoung',
-      //     img_url: '',
-      //     introduce:
-      //       '나의 소개입니다! 나의 소개입니다! 나의 소개입니다! 나의 소개입니다! 나의 소개입니다! 나의 소개입니다! 나의 소개입니다!',
-      //     tier: 'RED',
-      //     total_volunteer_hours: 120,
-      //     total_volunteer_count: 9,
-      //     detail: {
-      //       name: '김주영',
-      //       email: 'djm06294@naver.com',
-      //       gender: 'female',
-      //       birthDate: '1999-03-01',
-      //       contactNumber: '010-1222-2222'
-      //     }
-      //   });
     };
     fetchData();
   }, []);
