c78cb90d336d0ba56e6cf8e5dcb30f8528c1fd93
710minjoon <66014783+minjoon97@users.noreply.github.com>
2024-12-09 12:21:33 +0900

    ✨feat: notification수정 (#263)
    Co-authored-by: Minjoon Kim <annawa678@gmail.com>

--- src/components/alert-item/index.tsx
+++ src/components/alert-item/index.tsx
@@ -5,23 +5,31 @@ import { AlertType } from '@/shared/types/alert-type/AlertType';
 interface AlertItemProps {
   item: AlertType;
   singleRead: (id: number) => void;
+  fetchNotifications: () => Promise<void>;
 }
 
-const AlertItem: React.FC<AlertItemProps> = ({ item, singleRead }) => {
+const AlertItem: React.FC<AlertItemProps> = ({ item, singleRead, fetchNotifications }) => {
   const navigate = useNavigate();
 
   return (
     <Wrapper
       onClick={() => {
-        if (item.type === 'VOLUNTEER_APPLY_STATUS_CHANGE') navigate(`/aidrqdetail/${item.related_id}`);
+        if (item.type === 'VOLUNTEER_APPLY_STATUS_CHANGE' || item.type === 'VOLUNTEER_REVIEW_REQUEST')
+          navigate(`/aidrqdetail/${item.related_id}`);
+        else if (item.type === 'NOTE_BLAH_BLAH') navigate(`/mypage`);
       }}>
       <p>{item.type}</p>
       <p>{item.title}</p>
       <div>
         <p>{item.created_at}</p>
         <button
-          onClick={() => {
-            singleRead(item.notification_id);
+          onClick={(e) => {
+            const handleClick = async () => {
+              e.stopPropagation();
+              await singleRead(item.id);
+              await fetchNotifications();
+            };
+            handleClick();
           }}>
           삭제
         </button>
