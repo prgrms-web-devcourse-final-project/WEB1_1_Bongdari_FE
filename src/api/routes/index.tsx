cd4e0c797a4a5284d4c9c67584e6a3a1688db170
Minjoon Kim <annawa678@gmail.com>
2024-12-09 16:36:50 +0900

    ✨feat:랜딩페이지연결

--- src/api/routes/index.tsx
+++ src/api/routes/index.tsx
@@ -37,6 +37,14 @@ const MyPage = () => {
   }
 };
 
+const HomeRouter = () => {
+  // localStorage 체크
+  const isFirstVisit = localStorage.getItem('login-storage') !== null;
+
+  // 로그인 상태에 따라 리다이렉션
+  return isFirstVisit ? <Navigate to="/main" replace /> : <Navigate to="/landing" replace />;
+};
+
 const routes: RouteObject[] = [
   {
     path: '/',
@@ -44,6 +52,10 @@ const routes: RouteObject[] = [
     children: [
       {
         path: '/',
+        element: <HomeRouter />
+      },
+      {
+        path: '/main',
         element: <MainPage />
       },
       {
