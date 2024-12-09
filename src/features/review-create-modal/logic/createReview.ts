92aaaa8ffc33f7ec53e3a7b2bdf7b26aef84c668
Minjoon Kim <annawa678@gmail.com>
2024-12-09 11:29:41 +0900

    ✨feat:리뷰 수정

--- src/features/review-create-modal/logic/createReview.ts
+++ src/features/review-create-modal/logic/createReview.ts
@@ -1,7 +1,17 @@
 import axiosInstance from '@/api/apis';
 
 export const createReview = async (formData: FormData) => {
-  console.log('여기까지는 옴');
-  const response = await axiosInstance.post('/api/review', formData);
-  return response.data;
+  try {
+    const response = await axiosInstance.post('/api/recruit-board', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    return response;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+    throw new Error('리뷰 작성에 실패했습니다.');
+  }
 };
