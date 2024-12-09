32762711c21bf949e7750bb067dc121c1f8472f3
Jinny <asdfghjk1052@naver.com>
2024-12-09 18:01:48 +0900

    ✨ [#269] Feat: 기관 컨텍 페이지 퍼블리싱 완성 (#280)

---
+++ src/pages/center-contact-page/ui/contact-description/index.tsx
@@ -0,0 +1,54 @@
+import {
+  Circle,
+  Description,
+  DescriptionItem,
+  DescriptionTitle,
+  DescriptionWrapper,
+  SectionBox,
+  Text
+} from './indexCss';
+
+const ContactDescription = () => {
+  return (
+    <DescriptionWrapper>
+      <DescriptionTitle>기관 인증 요청</DescriptionTitle>
+      <SectionBox>
+        <DescriptionItem>
+          <Circle>
+            <Text>폼 제출</Text>
+          </Circle>
+        </DescriptionItem>
+        <DescriptionItem>
+          <Circle>
+            <Text>검토 과정</Text>
+          </Circle>
+        </DescriptionItem>
+        <DescriptionItem>
+          <Circle>
+            <Text>계정 발급</Text>
+          </Circle>
+        </DescriptionItem>
+        <DescriptionItem>
+          <Circle>
+            <Text>완료</Text>
+          </Circle>
+        </DescriptionItem>
+      </SectionBox>
+      <SectionBox>
+        <Description>
+          기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을
+          발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토
+          과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수
+          있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐
+          계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출
+          후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수
+          있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐
+          계정을 발급 받을 수 있습니다.기관은 폼 제출 후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.기관은 폼 제출
+          후, 검토 과정을 거쳐 계정을 발급 받을 수 있습니다.
+        </Description>
+      </SectionBox>
+    </DescriptionWrapper>
+  );
+};
+
+export default ContactDescription;
