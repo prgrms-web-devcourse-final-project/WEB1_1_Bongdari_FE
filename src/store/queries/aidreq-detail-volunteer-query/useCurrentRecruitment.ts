030c9b1e2d5c459333c1ba4d6cbe6b9c279e761d
710minjoon <annawa6@naver.com>
2024-12-24 15:20:47 +0900

    ✨feat: aidrq detail기능 reactquery로 변경

--- src/store/queries/aidreq-detail-volunteer-query/useCurrentRecruitment.ts
+++ src/store/queries/aidreq-detail-volunteer-query/useCurrentRecruitment.ts
@@ -1,6 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '@/api/apis';
+import { RecruitmentState } from '@/shared/types/recruitment-state/recruitmentStateType';
 
 export const fetchCurrentRecruitment = async (id: number) => {
   const response = await axiosInstance.get(`/api/volunteer-apply/recruit-board/${id}/summary`);
   return response.data;
+};
+
+export const useCurrentRecruitment = (id: number) => {
+  return useQuery<RecruitmentState>({
+    queryKey: ['currentRecruitment', id],
+    queryFn: () => fetchCurrentRecruitment(id),
+    enabled: !!id
+  });
 };
--- src/store/queries/aidreq-detail-volunteer-query/useFetchCenterProfile.ts
+++ src/store/queries/aidreq-detail-volunteer-query/useFetchCenterProfile.ts
@@ -1,10 +1,20 @@
 import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
-export const fetchCenterProfileForAidRq = async (centerId: string) => {
+import { centerProfileType } from '@/shared/types/center-profile/centerProfile';
+
+const fetchCenterProfileForAidRq = async (centerId: string) => {
   try {
     const response = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/center/profile/${centerId}`);
     return response.data.data;
   } catch (error) {
     console.error('Error:', error);
   }
+};
+
+export const useFetchCenterProfileForAidRq = (centerId: string) => {
+  return useQuery<centerProfileType>({
+    queryKey: ['centerProfile', centerId],
+    queryFn: () => fetchCenterProfileForAidRq(centerId)
+  });
 };
