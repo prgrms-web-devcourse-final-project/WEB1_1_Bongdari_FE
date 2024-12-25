890f6a10e6ae90b7093c9e908f8c34f322b8047a
youjin-hong <asdfghjk1052@naver.com>
2024-12-25 21:45:43 +0900

    ♻️ [#314] Refactor: 버튼 컴포넌트 통합

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,4 +1,19 @@
-import OtherButton from './other-button';
-import SubmitButton from './submit-button';
+import ButtonComponent from './indexCss';
 
-export { OtherButton, SubmitButton };
+interface ButtonProps {
+  onClick?: () => void;
+  label: string;
+  disabled?: boolean;
+  type?: 'blue' | 'white';
+  className?: string;
+}
+
+const Button = ({ onClick = () => {}, label, disabled = false, type = 'blue', className }: ButtonProps) => {
+  return (
+    <ButtonComponent onClick={onClick} disabled={disabled} $type={type} className={className}>
+      {label}
+    </ButtonComponent>
+  );
+};
+
+export default Button;
