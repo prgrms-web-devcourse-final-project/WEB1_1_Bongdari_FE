3a7216360d90105a5b074cf9d3f7a1ade546e9c0
710minjoon <annawa6@naver.com>
2024-12-27 11:34:06 +0900

    ✨feat:main에서 관심기관 리스트 불러와서 저장

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -8,23 +8,20 @@ import Ranking from './_components/ranking';
 import Community from './_components/community';
 import { useLoginStore } from '@/store/stores/login/loginStore';
 import axiosInstance from '@/api/apis';
+import useInterestStore from '@/store/stores/interest-center/interestStore';
 // import LocalTestBox from './_components/localTestBox';
 // import LocalTestBoxVolun from './_components/localTestBoxVolun';
 
 export default function MainPage() {
   const loginType = useLoginStore((state) => state.loginType);
   const setLoginInfo = useLoginStore((state) => state.setLoginInfo);
+  const setCenterIds = useInterestStore((state) => state.setCenterIds);
 
   useEffect(() => {
-    //
-    //
-    //
-    //로컬개발환경에서 주석을 달고 사용하세요.
+    // 응답으로 받은 로그인 정보를 zustand에 저장
     const getLoginInfo = async () => {
       try {
         const response = await axiosInstance.get('/api/token/userinfo');
-        // 응답으로 받은 로그인 정보를 zustand에 저장
-        console.log(response.data);
         const USER_ID = response.data.USER_ID;
         const ROLE = response.data.ROLE;
         if (ROLE === 'ROLE_CENTER' || ROLE === 'ROLE_VOLUNTEER') setLoginInfo(USER_ID, ROLE);
@@ -34,6 +31,23 @@ export default function MainPage() {
     };
     getLoginInfo();
   }, []);
+
+  useEffect(() => {
+    // 응답으로 받은 관심기관 리스트를 zustand에 저장
+    const getInterestCenter = async () => {
+      try {
+        const response = await axiosInstance.get('/api/interest-centers');
+        const centerList = response.data;
+        console.log('관심기관 리스트', centerList);
+        setCenterIds(centerList);
+      } catch (error) {
+        console.error('로그인 정보 가져오기 실패:', error);
+      }
+    };
+    if (loginType === 'ROLE_VOLUNTEER') {
+      getInterestCenter();
+    }
+  }, [loginType]);
 
   return (
     <Wrapper>
