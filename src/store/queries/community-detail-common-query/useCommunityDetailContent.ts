54cfe0ff4e5db11c6800b212e3243fc0f8ab91a1
jooyoung <jykimjbr860@gmail.com>
2024-12-28 16:38:50 +0900

    🐛 Bug: 커뮤니티 작성자 정보 안뜨는 이슈 해결

--- src/store/queries/community-detail-common-query/useCommunityDetailContent.ts
+++ src/store/queries/community-detail-common-query/useCommunityDetailContent.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '@/api/apis';
-import { communityDetailType } from '@/shared/types/community-type/CommuntiyTypes';
-import { resType } from '@/shared/types/resType';
+// import { communityDetailType } from '@/shared/types/community-type/CommuntiyTypes';
+// import { resType } from '@/shared/types/resType';
 
 const fetchCommunityDetailContent = async (content_id: number) => {
-  const res: resType<communityDetailType> = await axiosInstance.get(`/api/community-board/${content_id}`);
+  const res = await axiosInstance.get(`/api/community-board/${content_id}`);
   return res.data;
 };
 
