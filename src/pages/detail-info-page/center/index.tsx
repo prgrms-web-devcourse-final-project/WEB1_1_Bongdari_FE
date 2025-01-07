1f38687b5c15838c1c604f515a5aba64a1990315
youjin-hong <asdfghjk1052@naver.com>
2025-01-07 10:29:01 +0900

    ✨ [#399] Feat: 기관 상세정보 입력 폼 퍼블리싱 완료

--- src/pages/detail-info-page/center/index.tsx
+++ src/pages/detail-info-page/center/index.tsx
@@ -1,15 +1,15 @@
 import InputBox from '@/components/inputBox';
-import { SubmitButton } from './indexCss';
+import { DetailInfoForm, PageWrapper, SubmitButton, TitleContainer } from './indexCss';
 
 const CenterDetailInfoPage = () => {
   return (
-    <div>
-      <div>
+    <PageWrapper>
+      <TitleContainer>
         {/* TODO: 통합하면 loginType에 따라 기관/봉사자로 title 구분하기 */}
         <h1>처음 방문하는 기관이신가요?</h1>
         <p>아래 정보를 입력해주세요.</p>
-      </div>
-      <form onSubmit={() => console.log('기관상세정보입력폼제출')}>
+      </TitleContainer>
+      <DetailInfoForm onSubmit={() => console.log('기관상세정보입력폼제출')}>
         <div>
           <label htmlFor="centerName">기관명</label>
           <InputBox colortype="white" textType="text" placeholder="기관명을 입력해주세요." />
@@ -24,8 +24,8 @@ const CenterDetailInfoPage = () => {
         </div>
 
         <SubmitButton label="입력하기" type="blue" />
-      </form>
-    </div>
+      </DetailInfoForm>
+    </PageWrapper>
   );
 };
 
