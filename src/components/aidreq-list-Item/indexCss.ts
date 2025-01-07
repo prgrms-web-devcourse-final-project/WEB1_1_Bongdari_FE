98db790b750b2d93553f3469481d9a9257264cde
710minjoon <annawa6@naver.com>
2025-01-07 11:38:03 +0900

    ✨feat:aidrqlistpage 반응형 완료

--- src/components/aidreq-list-Item/indexCss.ts
+++ src/components/aidreq-list-Item/indexCss.ts
@@ -7,10 +7,16 @@ export const Wrapper = styled.li<{ width: string }>`
   background-color: white;
   border-radius: ${theme.box.item.borderRadius};
   transition: 0.2s;
+  transition-property: border, box-shadow;
 
   &:hover {
     cursor: pointer;
     border: 1px solid rgba(0, 0, 0, 0.2);
     box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  @media (max-width: 1000px) {
+    transition-property: border, box-shadow;
+    width: 100%;
   }
 `;
