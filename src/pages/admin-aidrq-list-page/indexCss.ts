08d1796f3d87fa0dca0c05c87e871872dc9bb65c
youjin-hong <asdfghjk1052@naver.com>
2025-01-08 14:13:26 +0900

    ✨ [#421] Feat: 타이틀 반응형 적용

--- src/pages/admin-aidrq-list-page/indexCss.ts
+++ src/pages/admin-aidrq-list-page/indexCss.ts
@@ -10,10 +10,19 @@ export const PageWrapper = styled.div`
   padding: 250px 0;
   gap: 56px;
   align-items: center;
+
+  @media (max-width: 1000px) {
+    width: 90%;
+  }
 `;
 
 export const Title = styled.p`
   font-size: ${theme.fontSize.secondSize};
   font-weight: 700;
   text-align: center;
+
+  @media (max-width: 1000px) {
+    font-size: ${theme.fontSize.fifthSize};
+    width: 100%;
+  }
 `;
