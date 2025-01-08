05645624a365eb8342443de73e29254204e0d20f
youjin-hong <asdfghjk1052@naver.com>
2025-01-08 11:10:45 +0900

    ✨ [#419] Feat: 모달 반응형 적용

--- src/components/modal/indexCss.ts
+++ src/components/modal/indexCss.ts
@@ -17,15 +17,19 @@ export const ModalWrapper = styled.div`
 `;
 
 export const ModalContainer = styled.div<{ variant: ModalVariant; width?: string; padding?: string }>`
+  position: relative;
   background-color: #ffffff;
   border-radius: ${theme.modal.borderRadius};
   width: ${({ width }) => width || '90%'};
   max-width: ${({ variant }) =>
     variant === 'big' ? theme.modal.variants.big.width : theme.modal.variants.small.width};
   height: ${({ variant }) => (variant === 'big' ? theme.modal.variants.big.height : theme.modal.variants.small.height)};
-  position: relative;
   flex-direction: column;
   z-index: 100;
+
+  @media (max-width: 1000px) {
+    width: 90%;
+  }
 `;
 
 export const CloseButton = styled.button`
@@ -37,4 +41,10 @@ export const CloseButton = styled.button`
   height: 42px;
   top: 0;
   right: -4rem;
+
+  @media (max-width: 1000px) {
+    top: 4px;
+    right: 4px;
+    background-color: transparent;
+  }
 `;
