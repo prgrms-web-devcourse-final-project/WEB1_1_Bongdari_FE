e131d08e8db60e4ab421e9606d04e2bd8244b696
Jinny <asdfghjk1052@naver.com>
2025-01-08 16:21:39 +0900

    ✨ [#428] Feat: 기관이 조회하는 리뷰/쪽지 모달 반응형 완료 (#430)

--- src/features/review-read-modal/indexCss.ts
+++ src/features/review-read-modal/indexCss.ts
@@ -9,14 +9,27 @@ export const ModalContentWrapper = styled.div`
   height: 100%;
   justify-content: space-around;
   margin: auto;
-  /* padding: 50px 68px 31px 77px; */
   padding: 1rem;
+  overflow: hidden;
+
+  @media (max-width: 1000px) {
+    justify-content: flex-start;
+    align-items: stretch;
+    height: 100%;
+  }
 `;
 
 export const ScrollSection = styled.div`
   max-height: 100%;
   overflow-y: auto; // 세로 스크롤
-  padding: 50px;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+
+  @media (max-width: 1000px) {
+    padding: 2rem 1rem;
+  }
 
   &::-webkit-scrollbar {
     width: 8px;
@@ -47,6 +60,10 @@ export const ReviewTitle = styled.h1`
   font-size: ${theme.fontSize.fourthSize};
   font-weight: 600;
   cursor: default;
+
+  @media (max-width: 1000px) {
+    font-size: ${theme.fontSize.sixthSize};
+  }
 `;
 
 export const CreatedAt = styled.p`
@@ -62,6 +79,10 @@ export const ReviewImgBox = styled.div`
   background-color: #e9e9e9;
   overflow: hidden;
   margin: 38px auto 0;
+
+  @media (max-width: 1000px) {
+    margin: 20px 0 18px 0;
+  }
 `;
 
 export const ReviewImg = styled.img`
@@ -72,6 +93,10 @@ export const ReviewImg = styled.img`
 
 export const ReviewContent = styled.div`
   padding: 45px 0;
+
+  @media (max-width: 1000px) {
+    font-size: ${theme.fontSize.eighthSize};
+  }
 `;
 
 export const ProfileBox = styled.div`
@@ -81,11 +106,22 @@ export const ProfileBox = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 24px 31px 23px 35px;
+  margin-top: auto;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    flex-direction: column;
+    gap: 29px;
+  }
 `;
 
 export const ProfileInfo = styled.div`
   display: flex;
   align-items: center;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+  }
 `;
 
 export const ImgWrapper = styled.div`
@@ -107,6 +143,10 @@ export const NickName = styled.p`
   font-weight: 600;
   padding: 0 14px 0 24px;
   cursor: default;
+
+  @media (max-width: 1000px) {
+    padding: 0 14px;
+  }
 `;
 
 export const GloveImg = styled.img`
@@ -121,4 +161,9 @@ export const GoToProfileButton = styled(Button)`
   font-size: ${theme.fontSize.eighthSize};
   font-weight: 600;
   border-radius: 13px;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    height: 40px;
+  }
 `;
