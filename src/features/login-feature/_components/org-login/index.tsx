8b6b1d5ebb5bb3c09e0765878cd874542711a2e9
710minjoon <annawa6@naver.com>
2025-01-09 16:57:02 +0900

    🐛fix:cicd디버깅

--- src/features/login-feature/_components/org-login/index.tsx
+++ src/features/login-feature/_components/org-login/index.tsx
@@ -1,48 +1,48 @@
-import { OrgLoginCss } from './indexCss';
-import InputBox from '@/components/inputBox';
-import { useOrgLogin } from './logic/useOrgLogin';
-import Button from '@/components/button';
+// import { OrgLoginCss } from './indexCss';
+// import InputBox from '@/components/inputBox';
+// import { useOrgLogin } from './logic/useOrgLogin';
+// import Button from '@/components/button';
 
-const OrgLogin = () => {
-  const { idErr, pwdErr, checkId, checkPwd, onClickFindAcount, onClickLogin, onClickFirstVisit } = useOrgLogin();
-  return (
-    <OrgLoginCss>
-      <div className="inputWrap">
-        <i className="label">아이디</i>
+// const OrgLogin = () => {
+//   const { idErr, pwdErr, checkId, checkPwd, onClickFindAcount, onClickLogin, onClickFirstVisit } = useOrgLogin();
+//   return (
+//     <OrgLoginCss>
+//       <div className="inputWrap">
+//         <i className="label">아이디</i>
 
-        <InputBox
-          // inputBox 고쳐주세요!!
-          // height="60px"
-          colortype="white"
-          placeholder="아이디를 입력해주세요."
-          getInputText={checkId} // 필요 없는데 필수props라서 넣어줌
-          setFunc={checkId}
-        />
-        <i className="checkErr">{idErr}</i>
-      </div>
-      <div className="inputWrap">
-        <i className="label">비밀번호</i>
-        <InputBox
-          // inputBox 고쳐주세요!!
-          // height="60px"
-          colortype="white"
-          textType="password"
-          placeholder="비밀번호를 입력해주세요."
-          getInputText={checkPwd} // 필요 없는데 필수props라서 넣어줌
-          setFunc={checkPwd}
-        />
-        <i className="checkErr">{pwdErr}</i>
-      </div>
-      <div className="btnWrap">
-        <button className="findAccountBtn" onClick={onClickFindAcount}>
-          계정 찾기
-        </button>
-        <Button label="로그인" onClick={onClickLogin} />
-      </div>
-      <button className="firstVisitBtn" onClick={onClickFirstVisit}>
-        첫방문이신가요?
-      </button>
-    </OrgLoginCss>
-  );
-};
-export default OrgLogin;
+//         <InputBox
+//           // inputBox 고쳐주세요!!
+//           // height="60px"
+//           colortype="white"
+//           placeholder="아이디를 입력해주세요."
+//           getInputText={checkId} // 필요 없는데 필수props라서 넣어줌
+//           setFunc={checkId}
+//         />
+//         <i className="checkErr">{idErr}</i>
+//       </div>
+//       <div className="inputWrap">
+//         <i className="label">비밀번호</i>
+//         <InputBox
+//           // inputBox 고쳐주세요!!
+//           // height="60px"
+//           colortype="white"
+//           textType="password"
+//           placeholder="비밀번호를 입력해주세요."
+//           getInputText={checkPwd} // 필요 없는데 필수props라서 넣어줌
+//           setFunc={checkPwd}
+//         />
+//         <i className="checkErr">{pwdErr}</i>
+//       </div>
+//       <div className="btnWrap">
+//         <button className="findAccountBtn" onClick={onClickFindAcount}>
+//           계정 찾기
+//         </button>
+//         <Button label="로그인" onClick={onClickLogin} />
+//       </div>
+//       <button className="firstVisitBtn" onClick={onClickFirstVisit}>
+//         첫방문이신가요?
+//       </button>
+//     </OrgLoginCss>
+//   );
+// };
+// export default OrgLogin;
