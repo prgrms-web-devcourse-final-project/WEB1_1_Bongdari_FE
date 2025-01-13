bdfce597e36fdb9edca8f3d0da8e2bc33d7590d4
jooyoung <jykimjbr860@gmail.com>
2025-01-13 14:52:52 +0900

    💄 Style: 개인 마이페이지 반응형 진행중

--- src/pages/personal-my-page/indexCss.ts
+++ src/pages/personal-my-page/indexCss.ts
@@ -1,6 +1,5 @@
 import theme from '@/styles/theme';
 import styled from 'styled-components';
-// import theme from '@/styles/theme';
 
 export const Wrapper = styled.div`
   ${theme.defaultPageCss.defaultPageCss}
