eaf2aa7c12514f6b4f192a509bb545e6cab73fd6
youjin-hong <asdfghjk1052@naver.com>
2025-01-16 20:35:31 +0900

    🚑️ [#461] Feat: 정산하기 모달 반응형 완료

--- src/features/adjustment-modal/indexCss.ts
+++ src/features/adjustment-modal/indexCss.ts
@@ -10,6 +10,10 @@ export const ModalWrapper = styled.div`
   justify-content: space-around;
   margin: auto;
   padding: 1rem;
+
+  @media (max-width: 1000px) {
+    justify-content: space-between;
+  }
 `;
 
 export const ScrollSection = styled.div`
@@ -34,6 +38,10 @@ export const ScrollSection = styled.div`
   &::-webkit-scrollbar-thumb:hover {
     background: #555;
   }
+
+  @media (max-width: 1000px) {
+    padding: 1rem;
+  }
 `;
 
 export const TitleBox = styled.div`
@@ -45,6 +53,10 @@ export const TitleBox = styled.div`
 export const AdjustmentTitle = styled.p`
   font-size: ${theme.fontSize.fourthSize};
   font-weight: 600;
+
+  @media (max-width: 1000px) {
+    font-size: ${theme.fontSize.sixthSize};
+  }
 `;
 
 export const AdjustmentSubTitle = styled.p`
@@ -69,26 +81,47 @@ export const Applicant = styled.li`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  @media (max-width: 1000px) {
+    padding: 10px;
+  }
 `;
 
 export const NameBox = styled.div`
   display: flex;
   gap: 17px;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    gap: 5px;
+  }
 `;
 
 export const Name = styled.p`
   font-size: ${theme.fontSize.seventhSize};
+
+  @media (max-width: 1000px) {
+    font-size: ${theme.fontSize.eighthSize};
+  }
 `;
 
 export const NickName = styled.p`
   font-size: ${theme.fontSize.eighthSize};
   color: #a4a4a4;
+
+  @media (max-width: 1000px) {
+    font-size: ${theme.fontSize.ninthSize};
+  }
 `;
 
 export const ButtonBox = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+
+  @media (max-width: 1000px) {
+    padding-top: 1rem;
+  }
 `;
 
 export const AdjustmentButton = styled(Button)`
@@ -97,4 +130,9 @@ export const AdjustmentButton = styled(Button)`
   font-size: ${theme.fontSize.eighthSize};
   font-weight: 600;
   border-radius: 8px;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    height: 50px;
+  }
 `;
