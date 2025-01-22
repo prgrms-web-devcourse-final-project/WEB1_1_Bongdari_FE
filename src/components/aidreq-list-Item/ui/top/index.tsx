53766e38b9090edf13fea3f1aa23fdd412c367ab
youjin-hong <asdfghjk1052@naver.com>
2025-01-22 15:11:52 +0900

    🚑️ [#523] Feat: 모집글 수정/작성 test 완료, html 파싱 html-sanitize 설치

--- src/components/aidreq-list-Item/ui/top/index.tsx
+++ src/components/aidreq-list-Item/ui/top/index.tsx
@@ -1,3 +1,4 @@
+import sanitizeHtml from 'sanitize-html';
 import parse from 'html-react-parser';
 import AidRqCategoryLabel from '@/components/label/AidRqCategoryLabel';
 import { Wrapper, LabelContainer, Title, Text, Center } from './indexCss';
@@ -14,6 +15,23 @@ interface TopProps {
 }
 
 const Top = ({ title, content, center, category, admitted }: TopProps) => {
+  const sanitizedContent = sanitizeHtml(content, {
+    allowedTags: ['p', 'img', 'br', 'b', 'strong', 'em', 'ul', 'ol', 'li', 'a', 'span'],
+    allowedAttributes: {
+      '*': ['style'], // 모든 태그에 style 속성 허용
+      a: ['href', 'target'],
+      img: ['src', 'alt']
+    },
+    allowedStyles: {
+      '*': {
+        color: [/.*/],
+        'background-color': [/.*/],
+        'font-size': [/.*/],
+        'text-align': [/.*/]
+      }
+    }
+  });
+
   return (
     <Wrapper>
       <LabelContainer>
@@ -21,7 +39,7 @@ const Top = ({ title, content, center, category, admitted }: TopProps) => {
         {admitted && <AidRqCertifiedLabel></AidRqCertifiedLabel>}
       </LabelContainer>
       <Title>{title}</Title>
-      <Text>{parse(content)}</Text>
+      <Text>{parse(sanitizedContent)}</Text>
       <Center>{center?.name}</Center>
     </Wrapper>
   );
--- src/pages/aidrq-detail-page/ui/text-content/index.tsx
+++ src/pages/aidrq-detail-page/ui/text-content/index.tsx
@@ -1,4 +1,5 @@
 import parse from 'html-react-parser';
+import sanitizeHtml from 'sanitize-html';
 import { RecruitCount, Text, Wrapper } from './indexCss';
 import { AidRqDetailType } from '@/shared/types/aidrq-detail/aidrqDetailType';
 
@@ -7,10 +8,44 @@ interface AidRqDetailCenterProfileProps {
 }
 
 const TextContent: React.FC<AidRqDetailCenterProfileProps> = ({ data }) => {
+  const sanitizedContent = sanitizeHtml(data.content, {
+    allowedTags: [
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+      'p',
+      'img',
+      'br',
+      'b',
+      'strong',
+      'em',
+      'ul',
+      'ol',
+      'li',
+      'a',
+      'span'
+    ],
+    allowedAttributes: {
+      '*': ['style'], // 모든 태그에 style 속성 허용
+      a: ['href', 'target'],
+      img: ['src', 'alt']
+    },
+    allowedStyles: {
+      '*': {
+        color: [/.*/],
+        'background-color': [/.*/],
+        'font-size': [/.*/],
+        'text-align': [/.*/]
+      }
+    }
+  });
   return (
     <Wrapper>
       <div>
-        <Text>{parse(data.content)}</Text>
+        <Text>{parse(sanitizedContent)}</Text>
       </div>
       <RecruitCount>예상 모집 인원 : {data.recruitment_count}명</RecruitCount>
     </Wrapper>
