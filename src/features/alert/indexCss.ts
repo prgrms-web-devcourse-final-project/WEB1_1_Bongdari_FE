11fe97e5c7fa37969c7375b1b4c39a267c00e308
710minjoon <annawa6@naver.com>
2025-01-23 11:21:35 +0900

    ✨feat:알림창 높이고정 및 스크롤구현

--- src/features/alert/indexCss.ts
+++ src/features/alert/indexCss.ts
@@ -12,11 +12,31 @@ export const Wrapper = styled.div`
 export const Container = styled.div`
   width: 100%;
   height: 100%;
+  max-height: 400px;
   background-color: white;
   border: 1px solid #dfdfdf;
   border-radius: 12px;
   padding: 1rem;
   box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
 `;
 
 export const Top = styled.div`
