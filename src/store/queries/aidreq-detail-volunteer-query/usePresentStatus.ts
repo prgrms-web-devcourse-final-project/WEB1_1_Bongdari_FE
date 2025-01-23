5d4d24699f1e338385dac7c1d0e196b41d37063a
710minjoon <66014783+minjoon97@users.noreply.github.com>
2025-01-23 14:59:57 +0900

    ✨feat:지원현황조회api수정 (#539)

--- src/store/queries/aidreq-detail-volunteer-query/usePresentStatus.ts
+++ src/store/queries/aidreq-detail-volunteer-query/usePresentStatus.ts
@@ -1,20 +1,24 @@
+import axiosInstance from '@/api/apis';
 import { PresentResponse } from '@/shared/types/aidrq-detail/PresentResponse';
 import axios from 'axios';
 import { Dispatch, SetStateAction } from 'react';
 
+interface ApiResponse {
+  code: number;
+  message: string;
+  data: PresentResponse | null;
+}
+
 export const myPresentStatus = async (
   setPresentState: Dispatch<SetStateAction<PresentResponse | null>>,
-  volunteerId: string | null,
   requestId: string | undefined
 ) => {
   try {
-    const response = await axios.get(
-      `${import.meta.env.VITE_APP_BASE_URL}/api/volunteer-apply/recruit-board/${requestId}/volunteer/${volunteerId}`
-    );
-    console.log('현재모집글에 대한 나의 지원상태', response.data);
-    if (response.data.code === 200) {
-      setPresentState(response.data.data);
-    } else if (response.data.code === 210) {
+    const response = await axiosInstance.get<unknown, ApiResponse>(`/api/volunteer-apply/recruit-board/${requestId}`);
+    console.log('현재모집글에 대한 나의 지원상태', response);
+    if (response.code === 200) {
+      setPresentState(response.data);
+    } else if (response.code === 210) {
       setPresentState({ id: 0, status: 'none', attended: false, is_reviewed: false });
     }
   } catch (error: unknown) {
