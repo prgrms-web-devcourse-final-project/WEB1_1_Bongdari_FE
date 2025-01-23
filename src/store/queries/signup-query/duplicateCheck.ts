16214647450c5f1216600122372716d17ff4ca89
710minjoon <annawa6@naver.com>
2025-01-23 21:31:54 +0900

    🐛fix:중복확인api수정

--- src/store/queries/signup-query/duplicateCheck.ts
+++ src/store/queries/signup-query/duplicateCheck.ts
@@ -4,7 +4,7 @@ type IdCheckType = 'success' | 'error' | null;
 
 export const duplicateCheck = async (setIdCheck: React.Dispatch<React.SetStateAction<IdCheckType>>, id: string) => {
   try {
-    const response = await axiosInstance.get(`/api/user/exists/?accountId=${id}`);
+    const response = await axiosInstance.get(`/api/user/exists?accountId=${id}`);
     setIdCheck(response.data ? 'error' : 'success');
   } catch (error) {
     console.error('중복 확인 실패:', error);
