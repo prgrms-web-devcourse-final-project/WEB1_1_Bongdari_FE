ed9e63f7224f3861cf168022761350716c45dbc7
Jinny <asdfghjk1052@naver.com>
2025-02-05 15:43:53 +0900

    🐛 [#585] Fix: 기관이 조회하는 리뷰 모달에 volunteerId store에서 가져오기 (#586)
    지금 봉사자 프로필 데이터에 volunteerId 없음

--- src/store/queries/volunteer-profile/useFetchPersonProfile.ts
+++ src/store/queries/volunteer-profile/useFetchPersonProfile.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axiosInstance from '@/api/apis';
 
 const fetchPersonProfile = async (volunteerId: string) => {
-  const res = await axiosInstance.get(`/api/volunteer/profile/user-id/${volunteerId}`);
+  const res = await axiosInstance.get(`/api/volunteer/profile/volunteer-id/${volunteerId}`);
   return res.data;
 };
 
